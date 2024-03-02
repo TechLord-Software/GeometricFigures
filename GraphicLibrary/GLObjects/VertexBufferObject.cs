c706e29f4775a26fe4b440ce5b450c154d3a4080
D3nisPi <d3nis.p1shchik@gmail.com>
2024-03-02 17:46:00 +0300

    Обертки над объектами opengl

---
+++ GraphicLibrary/GLObjects/VertexBufferObject.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicLibrary.GLObjects
+{
+    public struct VertexBufferObject
+    {
+
+    }
+}
