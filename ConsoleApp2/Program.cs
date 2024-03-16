f0b6c05c795ab4f719289a7e5256009e4e53e2fc
D3nisPi <d3nis.p1shchik@gmail.com>
2024-03-16 11:50:36 +0300

    Тестовое окно

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using GraphicLibrary.Scenes;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace TestWindow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                Profile = ContextProfile.Compatability,
+                Size = new Vector2i(900, 900),
+            };
+            using (var testWindow = Scene.Default)
+            {
+                testWindow.Run();
+            }
+        }
+    }
+}
