5ec28368370a44a993b38200da78316eebfbda76
Sergey <sergey.vorobev_2004@mail.ru>
2024-03-16 12:33:53 +0300

    Добавление ещё одной сферы

--- GraphicLibrary/ComplexModels/ModelFigures.cs
+++ GraphicLibrary/ComplexModels/ModelFigures.cs
@@ -15,7 +15,8 @@ namespace GraphicLibrary.ComplexModels
         private static string cylinderPath = @"ComplexModels\Figures\Cylinder.obj";
         private static string icosahedronPath = @"ComplexModels\Figures\Icosahedron.obj";
         private static string octahedronPath = @"ComplexModels\Figures\Octahedron.obj";
-        private static string spherePath = @"ComplexModels\Figures\Sphere.obj";
+        private static string sphere1Path = @"ComplexModels\Figures\Sphere1.obj";
+        private static string sphere2Path = @"ComplexModels\Figures\Sphere2.obj";
         private static string squarePath = @"ComplexModels\Figures\square.obj";
         private static string tetrahedronPath = @"ComplexModels\Figures\Tetrahedron.obj";
         private static string thorPath = @"ComplexModels\Figures\Thor.obj";
@@ -27,7 +28,8 @@ namespace GraphicLibrary.ComplexModels
         private static Model cube = Model.Parse(cubePath);
         private static Model cylinder = Model.Parse(cylinderPath);
         private static Model octahedron = Model.Parse(octahedronPath);
-        private static Model sphere = Model.Parse(spherePath);
+        private static Model sphere1 = Model.Parse(sphere1Path);
+        private static Model sphere2 = Model.Parse(sphere2Path);
         private static Model icosahedron = Model.Parse(icosahedronPath);
         private static Model square = Model.Parse(squarePath);
         private static Model tetrahedron = Model.Parse(tetrahedronPath);
@@ -40,7 +42,8 @@ namespace GraphicLibrary.ComplexModels
         public static Model Cube { get; }
         public static Model Cylinder { get; }
         public static Model Octahedron { get; }
-        public static Model Sphere { get; }
+        public static Model Sphere1 { get; }
+        public static Model Sphere2 { get; }
         public static Model Icosahedron { get; }
         public static Model Square { get; }
         public static Model Tetrahedron { get; }
@@ -55,7 +58,8 @@ namespace GraphicLibrary.ComplexModels
             Cube = (Model)cube.Clone();
             Cylinder = (Model)cylinder.Clone();
             Octahedron = (Model)octahedron.Clone();
-            Sphere = (Model)sphere.Clone();
+            Sphere1 = (Model)sphere1.Clone();
+            Sphere2 = (Model)sphere2.Clone();
             Icosahedron = (Model)icosahedron.Clone();
             Square = (Model)square.Clone();
             Tetrahedron = (Model)tetrahedron.Clone();
